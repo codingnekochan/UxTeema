48a67f3ad2e3796083a1b45ff50207f29cc07592
Mariam Ibrahim <ibrahimoyiza198@gmail.com>
2025-01-07 00:22:03 +0100

    spacing fix

--- src/sections/projects.jsx
+++ src/sections/projects.jsx
@@ -2,7 +2,7 @@ import ProjectCardComponent from "../components/projectCardComponent"
 import DashIcon from '../assets/dash.svg'
 function ProjectsSection({ ZeeInfo }) {
     return (
-        <section id='projects' className="px-6 md:px-10 lg:px-14 xl:px-[120px] mt-[200px]">
+        <section id='projects' className="px-6 md:px-10 lg:px-14 xl:px-[120px] xl:mt-[200px]">
             <div className="flex justify-start items-center gap-4 md:gap-5 lg:gap-6 2xl:gap-7 mb-6 md:mb-10 2xl:mb-12">
                 <img src={DashIcon} alt="" className="w-6 md:w-7 lg:w-8 2xl:w-9" />
                 <h2 className="font-bold font-cabin tracking-[3%] leading-[120%] text-xl md:text-[26px]  lg:text-3xl 2xl:text-4xl">Project</h2>
